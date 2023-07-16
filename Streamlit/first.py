af9e22f0ce1ea23c93bd074720352ff455970138
MRPRDX <httdiyversion2.0.1@gmail.com>
2023-07-16 21:47:06 +0330

    commit

--- Streamlit/first.py
+++ Streamlit/first.py
@@ -1,3 +1,10 @@
 import streamlit as st
 st.title('Calculator')
 st.write("number 1")
+x = st.number_input("enter first number", step=1)
+# step = 1 is type (int)
+y = st.number_input("enter second number", step=1)
+n = x + y
+if st.button(label="Addition"):
+    st.write(n)
+
